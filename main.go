adc77ca9bf109665d6eea4ba9146cefdfb4b41c0
MRA11145 <44599975+MRA11145@users.noreply.github.com>
2024-05-06 08:40:12 +0530

    Added default page

--- main.go
+++ main.go
@@ -10,19 +10,32 @@ import (
 )
 
 func main() {
-	
+	// Initialize database
 	database.Init()
+
+	// Create a new mux router
 	r := mux.NewRouter()
 
+	// Serve static files from the "./web/static" directory
 	fs := http.FileServer(http.Dir("./web/static/"))
 	r.PathPrefix("/web/static/").Handler(http.StripPrefix("/web/static/", fs))
 
+	// Handle requests for the root URL ("/") by serving the index.html file
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/static/index.html")
+	})
+
+	// Define routes for the RESTful API
 	r.HandleFunc("/api/books", controller.GetBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", controller.GetBook).Methods("GET")
 	r.HandleFunc("/api/books", controller.CreateBooks).Methods("POST")
 	r.HandleFunc("/api/books/{id}", controller.UpdateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", controller.DeleteBook).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
-
+	// Start the HTTP server
+	log.Println("Listening on port 8000")
+	err := http.ListenAndServe(":8000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
